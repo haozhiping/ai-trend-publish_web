70e2a9535ff022b3afa915802e5319b9f268590d
cecilia <xuan.ervip@foxmail.com>
2025-07-06 00:26:32 +0800

    优化CSS架构符合Ant Design Pro规范

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#1677ff',
+          '@border-radius-base': '6px',
+          '@font-size-base': '14px',
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     proxy: {
