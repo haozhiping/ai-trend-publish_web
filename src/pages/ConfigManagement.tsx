5183fe9b40a3daf7b647377ba84220d77c49cdb4
cecilia <xuan.ervip@foxmail.com>
2025-07-06 15:07:36 +0800

    Fix GithubOutlined import error

--- src/pages/ConfigManagement.tsx
+++ src/pages/ConfigManagement.tsx
@@ -14,6 +14,7 @@ import {
   Divider
 } from 'antd'
 import { SaveOutlined, ReloadOutlined, EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons'
+import { SaveOutlined, ReloadOutlined, EyeInvisibleOutlined, EyeOutlined, GithubOutlined } from '@ant-design/icons'
 
 const { Option } = Select
 const { TextArea } = Input
